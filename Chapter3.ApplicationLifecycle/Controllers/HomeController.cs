70ef3e3382b03eca5c91369a8949c1bfee358ee1
Alexander Klassanov <klassanov@gmail.com>
2021-03-01 20:27:46 +0200

    HomeController and Index action added

---
+++ Chapter3.ApplicationLifecycle/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Chapter3.ApplicationLifecycle.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello, folks!";
+        }
+    }
+}
