d69a42a003313090f12d1c60eefc49360963d255
Alexander Klassanov <klassanov@gmail.com>
2021-04-02 19:26:19 +0300

    Handlers Demos + presentation changes + added presentation sequence file

---
+++ Chapter5.Handlers/Infrastructure/ChromeGrnHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+
+namespace Chapter5.Handlers.Infrastructure
+{
+    public class ChromeGrnHandler : IHttpHandler
+    {
+        public bool IsReusable => false;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            context.Response.Write("<h1>Hello from the ChromeGrnHandler</h1>");
+        }
+    }
+}
---
+++ Chapter5.Handlers/Infrastructure/DefaultGrnHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+
+namespace Chapter5.Handlers.Infrastructure
+{
+    public class DefaultGrnHandler : IHttpHandler
+    {
+        public bool IsReusable => false;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            context.Response.Write("<h1>Hello from the DefaultGrnHandler</h1>");
+        }
+    }
+}
