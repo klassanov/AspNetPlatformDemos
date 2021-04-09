e0eb5fdcedfc2fe4be91249e22383a9153036272
Alexander Klassanov <klassanov@gmail.com>
2021-04-09 21:39:34 +0300

    Demo 3.Modules project added

---
+++ Demo3.Modules/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Demo3.Modules.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Demo3.Modules/Modules/InfoModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace Demo3.Modules.Modules
+{
+    public class InfoModule : IHttpModule
+    {
+        private Stopwatch requestTimer;
+
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication application)
+        {
+            //Subscriptions done here
+            application.BeginRequest += (src, args) =>
+              {
+                  this.requestTimer = Stopwatch.StartNew();
+              };
+
+            application.EndRequest += HandleEndRequest;
+        }
+
+        private void HandleEndRequest(object src, EventArgs args)
+        {
+            var status = HttpContext.Current.Response.Status;
+            var browser = HttpContext.Current.Request.Browser.Browser;
+            var elapsedTime = requestTimer.ElapsedMilliseconds;
+            var infoMsg = $"Response status: {status}; Elapsed Time: {elapsedTime} ms; Browser: {browser}";
+            HttpContext.Current.Response.Write(infoMsg);
+            Debug.WriteLine(infoMsg);
+        }
+    }
+}
