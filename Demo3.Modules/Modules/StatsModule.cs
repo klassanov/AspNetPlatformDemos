000c7a63c541c02c6ef0970bbcb2b4ac78f68b89
Alexander Klassanov <klassanov@gmail.com>
2021-04-17 12:56:31 +0300

    StatsModule implemented

---
+++ Demo3.Modules/Modules/StatsModule.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace Demo3.Modules.Modules
+{
+    public class StatsModule : IHttpModule
+    {
+        private static List<string> requestUrls = new List<string>();
+        private static object lockObject = new object();
+
+
+        public void Init(HttpApplication app)
+        {
+            app.BeginRequest += (src, args) =>
+            {
+                lock (lockObject)
+                {
+                    if (app.Request.RawUrl.ToLower() == "/stats")
+                    {
+                        app.Response.Write($"")
+                    }
+                    else
+                    {
+                        requestUrls.Add(app.Request.RawUrl.ToLower());
+                    }
+                }
+            };
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
