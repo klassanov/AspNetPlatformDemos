34ec46104179091f3160d9a6889f59056d0a0271
Alexander Klassanov <klassanov@gmail.com>
2021-04-26 22:45:01 +0300

    Demo4.TracingRequests project added

---
+++ Demo4.TracingRequests/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Demo4.TracingRequests
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
